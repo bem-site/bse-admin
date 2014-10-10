4c526a9610d48494f18d926d78b06c5ca36f0b86
tormozz48 <tormozz48@gmail.com>
2014-10-10 22:52:40 +0400

    Add task for database snapshot creation

--- src/tasks/snapshot.js
+++ src/tasks/snapshot.js
@@ -1,5 +1,51 @@
-var vow = require('vow');
+var util = require('util'),
+    path = require('path'),
+
+    vow = require('vow'),
+    vowFs = require('vow-fs'),
+
+    levelDb = require('../level-db'),
+    logger = require('../logger');
+
+function getSnapshotName() {
+    var date = new Date();
+    return util.format('%s:%s:%s-%s:%s:%s',
+        date.getDate(),
+        date.getMonth() + 1,
+        date.getFullYear(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds()
+    );
+}
 
 module.exports = function(target) {
-    return vow.resolve(target);
+    logger.info('Start to create database snapshot', module);
+
+    if(!target.getChanges().areModified()) {
+        logger.warn('No changes were made during this synchronization. This step will be skipped', module);
+        return vow.resolve(target);
+    }
+
+    var snapshotName = getSnapshotName(),
+        snapshotPath = path.join(target.SNAPSHOTS_DIR, snapshotName);
+    return vowFs.makeDir(snapshotPath)
+        .then(function() {
+            return levelDb.copy(snapshotPath);
+        })
+        .then(function() {
+            var meta = {
+                date: snapshotName,
+                changes: target.getChanges()
+            };
+            return vowFs.write(path.join(snapshotPath, 'data.json'), JSON.stringify(meta, null, 4));
+        })
+        .then(function() {
+            logger.info(util.format('Database snapshot %s has been created successfully', snapshotName), module);
+            return vow.resolve(target);
+        })
+        .fail(function(err) {
+            logger.error(util.format('Database snapshot creation failed with error', err.message), module);
+            return vow.reject(err);
+        });
 };
