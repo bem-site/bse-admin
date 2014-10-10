c25b4aad43e889151fad83c644a9d7d8be7494ae
tormozz48 <tormozz48@gmail.com>
2014-10-10 22:47:10 +0400

    Add finalization task

---
+++ src/tasks/finalize.js
@@ -0,0 +1,8 @@
+var util = require('util'),
+    vow = require('vow'),
+    logger = require('../logger');
+
+module.exports = function(target) {
+    logger.info(util.format('FINAL: ||| %s ||| HAS BEEN FINISHED SUCCESSFULLY', target.getName()), module);
+    return vow.resolve(target);
+}
