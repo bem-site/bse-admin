6d4846aebf9feabdb282ac7b1f0aa1d02d72ae4e
tormozz48 <tormozz48@gmail.com>
2014-10-15 15:47:07 +0400

    Add tasks for create dynamic nodes for authors, translators and tags

---
+++ src/tasks/dynamic-people.js
@@ -0,0 +1,91 @@
+var util = require('util'),
+
+    vow = require('vow'),
+
+    logger = require('../logger'),
+    levelDb = require('../level-db'),
+    nodes = require('../model/nodes/index.js');
+
+/**
+ * Removes existed people nodes from database
+ * @param {TargetBase} target object
+ * @param {String} key - dynamic key for people parent node
+ * @returns {*}
+ */
+function removePeopleNodes(target, key) {
+    return levelDb
+        .getByCriteria(function(record) {
+            return record.value.dynamic === key;
+        })
+        .then(function(dynamicRecords) {
+            return vow.all(dynamicRecords.map(function(dynamicRecord) {
+                return levelDb.removeByCriteria(function(record) {
+                    return record.value.parent === dynamicRecord.value.id;
+                });
+            }));
+        });
+}
+/**
+ * Creates people nodes and saves them into database
+ * @param {TargetBase} target object
+ * @param {String} key - dynamic key for people parent node
+ * @returns {*}
+ */
+function addPeopleNodes(target, key) {
+    var dbPeopleKey = {
+            authors: target.KEY.AUTHORS,
+            translators: target.KEY.TRANSLATORS
+        }[key];
+
+    return vow.all([
+            levelDb.getByCriteria(function(record) {
+                return record.value.dynamic === key;
+            }),
+            levelDb.get(dbPeopleKey),
+            levelDb.getByKeyPrefix(target.KEY.PEOPLE_PREFIX)
+        ])
+        .spread(function(dynamicRecords, peopleKeys, peopleData) {
+            peopleData = peopleData.reduce(function(prev, record) {
+                prev[record.key.split(':')[1]] = record.value;
+                return prev;
+            }, {});
+
+            if(!dynamicRecords.length) {
+                logger.warn(util.format('No records for dynamic key %s were found in db', key), module);
+                return vow.resolve();
+            }
+
+            if(!peopleKeys.length) {
+                logger.warn('No people were collected', module);
+                return vow.resolve();
+            }
+
+            return vow.all(dynamicRecords.map(function(dynamicRecord) {
+                return vow.all(peopleKeys.map(function(person) {
+                    return (new nodes.person.PersonNode(dynamicRecord.value, person, peopleData[person])).saveToDb();
+                }));
+            }));
+        });
+}
+
+module.exports = function(target) {
+    logger.info('Start to create dynamic nodes for people', module);
+
+    var peopleKeys = ['authors', 'translators'];
+    return vow.all(peopleKeys.map(function(key) {
+            return removePeopleNodes(target, key);
+        }))
+        .then(function() {
+            return vow.all(peopleKeys.map(function(key) {
+                return addPeopleNodes(target, key);
+            }));
+        })
+        .then(function() {
+            logger.info('Successfully create dynamic nodes for people', module);
+            return vow.resolve(target);
+        })
+        .fail(function(err) {
+            logger.error(util.format('Creation of dynamic nodes for people failed with error %s', err.message), module);
+            return vow.reject(err);
+        });
+};
---
+++ src/tasks/dynamic-tags.js
@@ -0,0 +1,82 @@
+var util = require('util'),
+
+    vow = require('vow'),
+
+    logger = require('../logger'),
+    levelDb = require('../level-db'),
+    nodes = require('../model/nodes/index.js');
+
+
+/**
+ * Removes existed tag nodes from database
+ * @param {TargetBase} target object
+ * @param {String} key - dynamic key for tags parent node
+ * @returns {*}
+ */
+function removeTagNodes(target, key) {
+    return levelDb
+        .getByCriteria(function(record) {
+            return record.value.dynamic === key;
+        })
+        .then(function(dynamicRecords) {
+            return vow.all(dynamicRecords.map(function(dynamicRecord) {
+                return levelDb.removeByCriteria(function(record) {
+                    return record.value.parent === dynamicRecord.value.id;
+                });
+            }));
+        });
+}
+/**
+ * Creates tag nodes and saves them into database
+ * @param {TargetBase} target object
+ * @param {String} key - dynamic key for tags parent node
+ * @returns {*}
+ */
+function addTagNodes(target, key) {
+    var lang = key.split(':')[1];
+    return vow.all([
+            levelDb.getByCriteria(function(record) {
+                return record.value.dynamic === key;
+            }),
+            levelDb.get(target.KEY.TAGS)
+        ])
+        .spread(function(dynamicRecords, tags) {
+            if(!dynamicRecords.length) {
+                logger.warn(util.format('No records for dynamic key %s were found in db', key), module);
+                return vow.resolve();
+            }
+
+            if(!tags[lang].length) {
+                logger.warn(util.format('No tags were collected for lang %s', lang), module);
+                return vow.resolve();
+            }
+
+            return vow.all(dynamicRecords.map(function(dynamicRecord) {
+                return vow.all(tags[lang].map(function(tag) {
+                    return (new nodes.tag.TagNode(dynamicRecord.value, tag)).saveToDb();
+                }));
+            }));
+        });
+}
+
+module.exports = function(target) {
+    logger.info('Start to create dynamic nodes for tags', module);
+
+    var tagKeys = ['tags:en', 'tags:ru'];
+    return vow.all(tagKeys.map(function(key) {
+            return removeTagNodes(target, key);
+        }))
+        .then(function() {
+            return vow.all(tagKeys.map(function(key) {
+                return addTagNodes(target, key);
+            }));
+        })
+        .then(function() {
+            logger.info('Successfully create dynamic nodes for tags', module);
+            return vow.resolve(target);
+        })
+        .fail(function(err) {
+            logger.error(util.format('Creation of dynamic nodes for tags failed with error %s', err.message), module);
+            return vow.reject(err);
+        });
+};
