0b8801d0aa3f75f77c005b655f4540624007357b
tormozz48 <tormozz48@gmail.com>
2014-10-16 15:13:41 +0400

    Remove unused modules

--- src/controllers/testing.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var util = require('util'),
-    logger = require('../logger');
-
-module.exports = function(req, res) {
-    logger.info(util.format('testing controller action %s', req.path), module);
-    res.status(200).end('OK');
-};
