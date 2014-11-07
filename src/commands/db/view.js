5e214a06c435eb10488dcb61a64151f36796af75
tormozz48 <tormozz48@gmail.com>
2014-11-07 16:52:01 +0400

    Add path to database and port options to view-db command

--- src/commands/db/view.js
+++ src/commands/db/view.js
@@ -1,15 +1,33 @@
-var path = require('path'),
+var util = require('util'),
+    path = require('path'),
     logger = require('../../logger'),
     levelup  = require('levelup'),
-    LevelHUD = require('levelhud');
+    LevelHUD = require('levelhud'),
+
+    DEFAULT = {
+        DB_PATH: 'db/leveldb',
+        PORT: 3000
+    };
 
 module.exports = function () {
     return this
         .title('view data in database')
         .helpful()
-        .act(function () {
-            logger.info('Launch viewer fo data in database', module);
-            var poneys = levelup(path.join(process.cwd(), 'db', 'leveldb'), { encoding: 'json' });
-            new LevelHUD().use(poneys).listen(4420);
+        .opt()
+            .name('database').title('Path to database')
+            .short('db').long('database')
+            .def(DEFAULT.DB_PATH)
+            .end()
+        .opt()
+            .name('port').title('Port number')
+            .short('p').long('port')
+            .def(DEFAULT.PORT)
+            .end()
+        .act(function (opts) {
+            var database = path.join(process.cwd(), opts.database || DEFAULT.DB_PATH),
+                port = opts.port || DEFAULT.PORT;
+
+            logger.info(util.format('Launch viewer on port %s fo data in database %s', port, database), module);
+            return new LevelHUD().use(levelup(database, { encoding: 'json' })).listen(port);
         });
 };
