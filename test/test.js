0e8d513e3ae80f43e54efae1e375d7d57ec8a250
tormozz48 <tormozz48@gmail.com>
2015-02-06 15:05:45 +0400

    Add tests folder

---
+++ test/test.js
@@ -0,0 +1,7 @@
+describe('bem-data-source', function () {
+    describe('#publish', function () {
+        it('should be valid done', function (done) {
+            done();
+        });
+    });
+});
